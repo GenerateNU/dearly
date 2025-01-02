1ba3478f9c5586d0822365bd6333f194d69ee705
Stone Liu <liu.sto@northeastern.edu>
2025-01-02 16:56:11 -0500

    format

--- backend/src/types/api/schemas/users.ts
+++ backend/src/types/api/schemas/users.ts
@@ -1,6 +1,6 @@
 import { pathsSchema } from "../../../gen/generated_zod";
 import { components } from "../../../gen/schema";
-import z from "zod"
+import z from "zod";
 
 export type USER_API = components["schemas"]["User"];
 
@@ -8,10 +8,15 @@ export type USER_API = components["schemas"]["User"];
  * Generated Zod Schemas might need more manual processing for example removing the id, and Zod objects.
  * This is not ideal and should serve only as an example.
  */
-export const USER_BODY_ZOD =
-  pathsSchema.shape["/api/v1/users"].shape["post"].shape["requestBody"].unwrap().shape["content"]
-    .shape["application/json"].transform((schema) => { const { id, ...rest } = schema; return rest; }).pipe(
-      z.object({
-        firstName: z.string().min(1),
-        lastName: z.string().min(1)
-      }));
+export const USER_BODY_ZOD = pathsSchema.shape["/api/v1/users"].shape["post"].shape["requestBody"]
+  .unwrap()
+  .shape["content"].shape["application/json"].transform((schema) => {
+    const { id, ...rest } = schema;
+    return rest;
+  })
+  .pipe(
+    z.object({
+      firstName: z.string().min(1),
+      lastName: z.string().min(1),
+    }),
+  );
