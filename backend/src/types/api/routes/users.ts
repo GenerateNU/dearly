9c6c3e39f879d5b102b228561183888460b84027
Stone Liu <liu.sto@northeastern.edu>
2025-01-03 10:17:49 -0500

    Add Cleanup and typechecking

--- backend/src/types/api/routes/users.ts
+++ backend/src/types/api/routes/users.ts
@@ -3,9 +3,7 @@ import { USER_API } from "../schemas/users";
 import { API_ERROR } from "../schemas/error";
 import { paths } from "../../../gen/schema";
 
-export type CREATE_USER = TypedResponse<USER_API | API_ERROR>;
-export type GET_USER = TypedResponse<USER_API | API_ERROR>;
-export type PUT_USER = TypedResponse<USER_API | API_ERROR>;
+export type USER_RESPONSE = TypedResponse<USER_API | API_ERROR>;
 export type DEL_USER = TypedResponse<
   paths["/api/v1/users/me"]["delete"]["responses"]["200"]["content"]["application/json"] | API_ERROR
 >;
