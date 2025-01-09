87d1cb55af604bb80625cf64c6f2d13f8eaeff7b
mai <thisismainguyen@gmail.com>
2025-01-09 00:30:29 -0500

    fix frontend margin

--- frontend/app/(app)/(tabs)/index.tsx
+++ frontend/app/(app)/(tabs)/index.tsx
@@ -11,7 +11,6 @@ const Home = () => {
       justifyContent="center"
       backgroundColor="cardPrimaryBackground"
       flex={1}
-      margin="m"
     >
       <Button label="Logout" onPress={logout} />
     </Box>
