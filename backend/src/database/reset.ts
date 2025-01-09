71a610a1faad8de3b1c15944f1d1cc573468203e
Mai Nguyen <123816878+in-mai-space@users.noreply.github.com>
2025-01-09 12:35:16 -0500

    🤫 fix: suppress noisy logs in test

--- backend/src/database/reset.ts
+++ backend/src/database/reset.ts
@@ -13,16 +13,26 @@ import {
 } from "../entities/schema";
 
 export const resetDB = async (db: PostgresJsDatabase) => {
-  const tables = {
-    postsTable,
-    usersTable,
-    membersTable,
-    groupsTable,
-    notificationsTable,
-    invitationsTable,
-    linksTable,
-    likesTable,
-    commentsTable,
-  };
-  await reset(db, tables);
+  // suppress the noisy logs
+  const originalLog = console.log;
+  console.log = () => {};
+  try {
+    const tables = {
+      postsTable,
+      usersTable,
+      membersTable,
+      groupsTable,
+      notificationsTable,
+      invitationsTable,
+      linksTable,
+      likesTable,
+      commentsTable,
+    };
+
+    await reset(db, tables);
+  } catch (error) {
+    console.error("Error resetting the database:", error);
+  } finally {
+    console.log = originalLog;
+  }
 };
