c8b8193cd52921fa8688a8bea9bd5987070884ba
Mai Nguyen <123816878+in-mai-space@users.noreply.github.com>
2025-01-16 23:14:47 -0500

    fix: create global constants for validator's max min value (#68)

--- backend/src/entities/users/validator.ts
+++ backend/src/entities/users/validator.ts
@@ -2,19 +2,20 @@ import { usersTable } from "../schema";
 import { createInsertSchema, createUpdateSchema } from "drizzle-zod";
 import z from "zod";
 import { Expo } from "expo-server-sdk";
+import { MIN_LIMIT } from "../../constants/database";
 
 export type CreateUserPayload = typeof usersTable.$inferInsert;
 export type UpdateUserPayload = Partial<typeof usersTable.$inferInsert>;
 export type User = typeof usersTable.$inferSelect;
 
 export const createUserValidate = createInsertSchema(usersTable, {
-  name: (schema) => schema.min(1),
-  username: (schema) => schema.min(1),
+  name: (schema) => schema.min(MIN_LIMIT),
+  username: (schema) => schema.min(MIN_LIMIT),
 }).omit({ id: true });
 
 export const updateUserValidate = createUpdateSchema(usersTable, {
-  name: (schema) => schema.min(1),
-  username: (schema) => schema.min(1),
+  name: (schema) => schema.min(MIN_LIMIT),
+  username: (schema) => schema.min(MIN_LIMIT),
 }).omit({ id: true });
 
 export const expoTokenValidate = z.object({
