5f17aba2f2842403d8d35f9f53450c5421c023e6
mai <thisismainguyen@gmail.com>
2025-01-17 19:52:05 -0500

    seed group and member for testing

--- backend/src/tests/helpers/seed-db.ts
+++ backend/src/tests/helpers/seed-db.ts
@@ -1,10 +1,13 @@
 import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
-import { usersTable } from "../../entities/schema";
+import { groupsTable, membersTable, usersTable } from "../../entities/schema";
 import { CreateUserPayload } from "../../entities/users/validator";
-import { USER_ALICE_ID, USER_ANA_ID, USER_BOB_ID } from "./test-constants";
+import { DEARLY_GROUP_ID, USER_ALICE_ID, USER_ANA_ID, USER_BOB_ID } from "./test-constants";
+import { CreateGroupPayload } from "../../entities/groups/validator";
 
 export const seedDatabase = async (db: PostgresJsDatabase) => {
   await seedUser(db);
+  await seedGroup(db);
+  await seedMember(db);
 };
 
 const seedUser = async (db: PostgresJsDatabase) => {
@@ -35,3 +38,46 @@ const seedUser = async (db: PostgresJsDatabase) => {
     console.error("Error seeding database:", error);
   }
 };
+
+const seedGroup = async (db: PostgresJsDatabase) => {
+  const seedData: CreateGroupPayload[] = [
+    {
+      name: "dearly",
+      description: "dearly",
+      managerId: USER_ALICE_ID,
+      id: DEARLY_GROUP_ID
+    }
+  ];
+
+  try {
+    await db.insert(groupsTable).values(seedData);
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+}
+
+const seedMember = async (db: PostgresJsDatabase) => {
+  const seedData: typeof membersTable.$inferInsert[] = [
+    {
+      userId: USER_ALICE_ID,
+      groupId: DEARLY_GROUP_ID,
+      role: "MANAGER",
+    },
+    {
+      userId: USER_BOB_ID,
+      groupId: DEARLY_GROUP_ID,
+      role: "MEMBER",
+    },
+    {
+      userId: USER_ANA_ID,
+      groupId: DEARLY_GROUP_ID,
+      role: "MEMBER",
+    },
+  ];
+
+  try {
+    await db.insert(membersTable).values(seedData);
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+}
