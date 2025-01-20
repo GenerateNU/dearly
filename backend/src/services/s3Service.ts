8aafe8e73a715751c5806ede5bb8f325a07313ce
ZainabImadulla <zainab.imadulla@icloud.com>
2025-01-20 15:20:35 -0500

    audio compression method

--- backend/src/services/s3Service.ts
+++ backend/src/services/s3Service.ts
@@ -3,6 +3,7 @@ import { getConfigurations } from "../config/config"
 import { NotFoundError } from "../utilities/errors/app-error";
 import { randomUUIDv7 } from "bun";
 import sharp from "sharp"
+import * as lame from '@breezystack/lamejs';
 
 interface IS3Operations {
     // group is the uuid of the group this photo is being sent to (used as tagging number) -> make public group id number
@@ -63,6 +64,28 @@ class S3Impl implements IS3Operations {
 
         return new Blob(compressedImageArr)
     }
+
+    /**
+     * Will compress a given blob so that it takes up less storage and reduces load times
+     * @param file The audio blob that will be compressed
+     * @returns A promise of the blob is the newly compressed audio recording
+     */
+    async compressAudio(file: Blob, imageQuality: number = 80): Promise<Blob> {
+        var compressedAudio= [] // holds the final compressed audio
+        const mp3encoder = new lame.Mp3Encoder(1, 44100, 128); 
+        const audioBuffer: ArrayBuffer = await file.arrayBuffer()
+        const audioBufferInt16 = new Int16Array(audioBuffer, 0, Math.floor(audioBuffer.byteLength / 2));
+        var mp3Tmp = mp3encoder.encodeBuffer(audioBufferInt16); 
+
+        compressedAudio.push(mp3Tmp);
+        // flush the mp3encoder so that any remaining data is also pushed into the final 
+        // compressed audio
+        mp3Tmp = mp3encoder.flush()
+        compressedAudio.push(mp3Tmp)
+        return new Blob(compressedAudio)
+    }
+
+  
 
     async deleteObject(url: string) {
         const indexOfDotCom = url.indexOf(".com/")
