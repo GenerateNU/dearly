8311c56f7c16beaf3abe73c4155bbd29c09170f1
Stone Liu <liu.sto@northeastern.edu>
2025-01-20 22:00:31 -0500

    Dont sue me

--- backend/src/services/s3Service.ts
+++ backend/src/services/s3Service.ts
@@ -6,125 +6,127 @@ import sharp from "sharp"
 import * as lame from '@breezystack/lamejs';
 
 interface IS3Operations {
-    // group is the uuid of the group this photo is being sent to (used as tagging number) -> make public group id number
+  // group is the uuid of the group this photo is being sent to (used as tagging number) -> make public group id number
 
-    /**
-     * Will save a blob to s3 with some image type and and tag.
-     * @param file the file that will be saved to S3
-     * @param tag the tag that will be associated with the file
-     * @param fileType The type of the blob sent to the endpoint
-     * @returns Will return the url of the object in S3
-     */
-    saveObject(file: Blob, tag: string, fileType: "image" | "audio"): Promise<string | null>
+  /**
+   * Will save a blob to s3 with some image type and and tag.
+   * @param file the file that will be saved to S3
+   * @param tag the tag that will be associated with the file
+   * @param fileType The type of the blob sent to the endpoint
+   * @returns Will return the url of the object in S3
+   */
+  saveObject(file: Blob, tag: string, fileType: "image" | "audio"): Promise<string | null>
 
-    /**
-     * Will delete an object from S3 with the given S3 URL.
-     * @param url The URL of the object that will be deleted
-     * @throws NotFoundError: if the object was not able to be deleted
-     */
-    deleteObject(url: string): void
+  /**
+   * Will delete an object from S3 with the given S3 URL.
+   * @param url The URL of the object that will be deleted
+   * @throws NotFoundError: if the object was not able to be deleted
+   */
+  deleteObject(url: string): void
 }
 
 
 
 export default class S3Impl implements IS3Operations {
-    private client;
-    private bucketName
+  private client;
+  private bucketName
 
-    public constructor(client?: S3Client) {
-        const config = getConfigurations();
-        const region = config.s3Config.region;
-        const secretKey = config.s3Config.secretKey;
-        const publicKey = config.s3Config.publicKey;
-        this.bucketName = config.s3Config.name;
+  public constructor(client?: S3Client) {
+    const config = getConfigurations();
+    const region = config.s3Config.region;
+    const secretKey = config.s3Config.secretKey;
+    const publicKey = config.s3Config.publicKey;
+    this.bucketName = config.s3Config.name;
 
-        if (!client) {
-            this.client = new S3Client({
-                region: region,
-                credentials: {
-                    accessKeyId: publicKey,
-                    secretAccessKey: secretKey
-                }
-            })
+    if (!client) {
+      this.client = new S3Client({
+        region: region,
+        credentials: {
+          accessKeyId: publicKey,
+          secretAccessKey: secretKey
         }
+      })
+    } else {
+      this.client = client
     }
+  }
 
 
-    /**
-     * Will compress a given blob so that it takes up less storage and reduces load times
-     * NOTE: Use the optional argument: imageQuality if your image needs to have higher/lower quality
-     * In general small images can lower qualities but less than 40 is not recomended
-     * @param file The blob that will be compressed
-     * @param imageQuality The quality of the image [0, 100]
-     * @returns A promise of the blob is the newly compressed image
-     */
-    async compressImage(file: Blob, imageQuality: number = 80): Promise<Blob> {
-        const imageBuffer: ArrayBuffer = await file.arrayBuffer()
-        const compressedImageArr = await sharp(imageBuffer)
-            .jpeg({ quality: imageQuality })
-            .toArray()
+  /**
+   * Will compress a given blob so that it takes up less storage and reduces load times
+   * NOTE: Use the optional argument: imageQuality if your image needs to have higher/lower quality
+   * In general small images can lower qualities but less than 40 is not recomended
+   * @param file The blob that will be compressed
+   * @param imageQuality The quality of the image [0, 100]
+   * @returns A promise of the blob is the newly compressed image
+   */
+  async compressImage(file: Blob, imageQuality: number = 80): Promise<Blob> {
+    const imageBuffer: ArrayBuffer = await file.arrayBuffer()
+    const compressedImageArr = await sharp(imageBuffer)
+      .jpeg({ quality: imageQuality })
+      .toArray()
 
-        return new Blob(compressedImageArr)
+    return new Blob(compressedImageArr)
+  }
+
+  /**
+   * Will compress a given blob so that it takes up less storage and reduces load times
+   * @param file The audio blob that will be compressed
+   * @returns A promise of the blob is the newly compressed audio recording
+   */
+  async compressAudio(file: Blob): Promise<Blob> {
+    const compressedAudio = [] // holds the final compressed audio
+    const mp3encoder = new lame.Mp3Encoder(1, 44100, 128);
+    const audioBuffer: ArrayBuffer = await file.arrayBuffer()
+    const audioBufferInt16 = new Int16Array(audioBuffer, 0, Math.floor(audioBuffer.byteLength / 2));
+    let mp3Tmp = mp3encoder.encodeBuffer(audioBufferInt16);
+
+    compressedAudio.push(mp3Tmp);
+    // flush the mp3encoder so that any remaining data is also pushed into the final 
+    // compressed audio
+    mp3Tmp = mp3encoder.flush()
+    compressedAudio.push(mp3Tmp)
+    return new Blob(compressedAudio)
+  }
+
+  /**
+   * Will save a blob to s3 with some image type and and tag.
+   * @param file the file that will be saved to S3
+   * @param tag the tag that will be associated with the file
+   * @param fileType The type of the blob sent to the endpoint
+   * @returns Will return the url of the object in S3
+   */
+  async saveObject(file: Blob, tag: string, fileType: "image" | "audio"): Promise<string | null> {
+    // to-do: compress blob, and deal with permission
+    const objectKey: string = randomUUIDv7()
+    const compressedFile = fileType == "image" ? await this.compressImage(file) : await this.compressAudio(file)
+    await this.client!.send(
+      new PutObjectCommand({
+        Bucket: this.bucketName,
+        Key: objectKey,
+        Body: compressedFile,
+        Tagging: `GroupID=${tag}`
+      })
+    )
+    const url: string = "https://" + this.bucketName + ".s3.amazonaws.com/" + objectKey;
+    return url;
+  }
+
+
+  /**
+   * Will delete an object from S3 with the given S3 URL.
+   * @param url The URL of the object that will be deleted
+   * @throws NotFoundError: if the object was not able to be deleted
+   */
+  async deleteObject(url: string) {
+    const indexOfDotCom = url.indexOf(".com/")
+    const objectKey: string = url.substring(indexOfDotCom + 5)
+    const res = await this.client!.send(
+      new DeleteObjectCommand({ Bucket: this.bucketName, Key: objectKey }),
+    );
+
+    if (res.$metadata.httpStatusCode! > 300) {
+      throw new NotFoundError(url)
     }
-
-    /**
-     * Will compress a given blob so that it takes up less storage and reduces load times
-     * @param file The audio blob that will be compressed
-     * @returns A promise of the blob is the newly compressed audio recording
-     */
-    async compressAudio(file: Blob): Promise<Blob> {
-        const compressedAudio = [] // holds the final compressed audio
-        const mp3encoder = new lame.Mp3Encoder(1, 44100, 128);
-        const audioBuffer: ArrayBuffer = await file.arrayBuffer()
-        const audioBufferInt16 = new Int16Array(audioBuffer, 0, Math.floor(audioBuffer.byteLength / 2));
-        let mp3Tmp = mp3encoder.encodeBuffer(audioBufferInt16);
-
-        compressedAudio.push(mp3Tmp);
-        // flush the mp3encoder so that any remaining data is also pushed into the final 
-        // compressed audio
-        mp3Tmp = mp3encoder.flush()
-        compressedAudio.push(mp3Tmp)
-        return new Blob(compressedAudio)
-    }
-
-    /**
-     * Will save a blob to s3 with some image type and and tag.
-     * @param file the file that will be saved to S3
-     * @param tag the tag that will be associated with the file
-     * @param fileType The type of the blob sent to the endpoint
-     * @returns Will return the url of the object in S3
-     */
-    async saveObject(file: Blob, tag: string, fileType: "image" | "audio"): Promise<string | null> {
-        // to-do: compress blob, and deal with permission
-        const objectKey: string = randomUUIDv7()
-        const compressedFile = fileType == "image" ? await this.compressImage(file) : await this.compressAudio(file)
-        await this.client!.send(
-            new PutObjectCommand({
-                Bucket: this.bucketName,
-                Key: objectKey,
-                Body: compressedFile,
-                Tagging: `GroupID=${tag}`
-            })
-        )
-        const url: string = "https://" + this.bucketName + ".s3.amazonaws.com/" + objectKey;
-        return url;
-    }
-
-
-    /**
-     * Will delete an object from S3 with the given S3 URL.
-     * @param url The URL of the object that will be deleted
-     * @throws NotFoundError: if the object was not able to be deleted
-     */
-    async deleteObject(url: string) {
-        const indexOfDotCom = url.indexOf(".com/")
-        const objectKey: string = url.substring(indexOfDotCom + 5)
-        const res = await this.client!.send(
-            new DeleteObjectCommand({ Bucket: this.bucketName, Key: objectKey }),
-        );
-
-        if (res.$metadata.httpStatusCode! > 300) {
-            throw new NotFoundError(url)
-        }
-    }
+  }
 }
--- backend/src/tests/services/s3Service.test.ts
+++ backend/src/tests/services/s3Service.test.ts
@@ -1,57 +1,23 @@
-import { Hono } from "hono";
-import { startTestApp } from "../helpers/test-app";
-import { TestBuilder } from "../helpers/test-builder";
-import { generateJWTFromID, generateUUID } from "../helpers/test-token";
-import { HTTPRequest, Status } from "../../constants/http";
-import { jest } from "@jest/globals";
-import Mock from "node:test"
-import S3Impl from "../../services/s3Service"
-import { S3Client } from "@aws-sdk/client-s3";
-import { mockClient } from "aws-sdk-client-mock"
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { mockClient } from 'aws-sdk-client-mock';
+import S3Impl from "../../services/s3Service";
+import fs from "fs"
+import { resolve } from 'node:path';
 
+const PROJECT_ROOT = resolve(__dirname, "../..")
 
-
-const mockedClient: jest.Mock<() => S3Client> = jest.fn()
-
-function testAddObject() {
-
-    const mS3Instance = {
-        deleteObject: jest.fn().mockReturnThis() as jest.Mock<(url: string) => void>,
-        saveObject: jest.fn().mockReturnThis() as jest.Mock<(file: Blob, tag: string, fileType: "image" | "audio") => Promise<string | null>>,
-        promise: jest.fn().mockReturnThis(),
-        catch: jest.fn()
-    };
-
-    jest.mock('aws-sdk', () => {
-        return { S3: jest.fn(() => mS3Instance) };
-    });
-
-    describe('should push object', () => {
-        it('should upload correctly', async () => {
-            const configService = {
-                get: jest
-                    .fn()
-                    .mockReturnValueOnce('accessKeyId')
-                    .mockReturnValueOnce('secretAccessKey')
-                    .mockReturnValueOnce('us-east')
-                    .mockReturnValueOnce('bucket-dev'),
-            };
-            mS3Instance.promise.mockResolvedValueOnce('fake response');
-            const s3Service = new S3Impl(configService);
-            const actual = await s3Service.upload('name', 'contentType', Buffer.from('ok'));
-            expect(actual).toEqual('fake response');
-            expect(mS3Instance.upload).toBeCalledWith({ Bucket: 'bucket-dev', Key: 'key', Body: Buffer.from('ok') });
-        });
-    });
-
-}
-
-
-class MockedS3 extends S3Client {
-    function put<InputType extends ClientInput, OutputType extends ClientOutput>(command: Command<ClientInput, InputType, ClientOutput, OutputType, SmithyResolvedConfiguration<HandlerOptions>>, options?: HandlerOptions): Promise<OutputType>;
-}
-
-
-
+describe("Save an object to s3 testing", () => {
+  it("Should return the valid s3 link", async () => {
+    const mockS3Client = mockClient(S3Client)
+    const client = mockS3Client.on(PutObjectCommand).resolves({ Size: 42069 }) as unknown as S3Client
+    let buffer = fs.readFileSync(PROJECT_ROOT + "/../../frontend/assets/icon.png");
+    let blob = new Blob([buffer]);
+    const s3Impl = new S3Impl(client);
+    const expected = await s3Impl.saveObject(blob, "test", "image");
+    const expectedString = "https://dearly.s3.amazonaws.com/"
+    expect(expected).not.toBeNull()
+    expect(expected).toContain(expectedString)
+  })
+})
 
 
