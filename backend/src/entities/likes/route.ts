38d17dda8f7ed0eaaaa75555297af07d0fc63d9d
Mai Nguyen <123816878+in-mai-space@users.noreply.github.com>
2025-01-21 17:27:04 -0500

    👍🏻 feat: like endpoints (#87)

---
+++ backend/src/entities/likes/route.ts
@@ -0,0 +1,18 @@
+import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { Hono } from "hono";
+import { LikeTransaction, LikeTransactionImpl } from "./transaction";
+import { LikeService, LikeServiceImpl } from "./service";
+import { LikeController, LikeControllerImpl } from "./controller";
+
+export const likeRoutes = (db: PostgresJsDatabase): Hono => {
+  const like = new Hono();
+
+  const likeTransaction: LikeTransaction = new LikeTransactionImpl(db);
+  const likeService: LikeService = new LikeServiceImpl(likeTransaction);
+  const postController: LikeController = new LikeControllerImpl(likeService);
+
+  like.patch("/", (ctx) => postController.toggleLike(ctx));
+  like.get("/", (ctx) => postController.getLikeUsers(ctx));
+
+  return like;
+};
