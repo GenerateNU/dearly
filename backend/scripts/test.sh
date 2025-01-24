bc7d17185f8e8a0e91f3e6b6af50e3511dd2653a
Stone Liu <132910172+stoneliuCS@users.noreply.github.com>
2025-01-23 19:04:06 -0500

    Testscript (#90)

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -1,19 +1,40 @@
 # Start the PostgreSQL container in the background
-docker-compose -f ./docker-compose.yml up -d # Use the correct relative path to docker-compose.yml
 timeout=5
 elapsed=0
 
-until pg_isready -h localhost -p 5432; do
-  if [ "$elapsed" -ge "$timeout" ]; then
-    echo "pg_isready timeout after $timeout seconds."
-    exit 1
+setup() {
+  docker-compose -f ./docker-compose.yml up -d # Use the correct relative path to docker-compose.yml
+}
+
+ready() {
+  until pg_isready -h localhost -p 5432; do
+    if [ "$elapsed" -ge "$timeout" ]; then
+      echo "pg_isready timeout after $timeout seconds."
+      exit 1
+    fi
+    sleep 1
+    elapsed=$((elapsed + 1))
+  done
+}
+
+teardown() {
+  # Stop and remove the Docker container after tests finish
+  docker-compose -f ./docker-compose.yml down --volumes
+}
+
+run() {
+  setup
+  ready
+  NODE_ENV=test
+  if [ $# -eq 0 ]; then
+    bun test
+  elif [ $# -eq 1 ]; then
+    bun test $1
+  elif [ $# -ge 2 ]; then
+    local test_name_pattern="${@:3}"
+    bun test --test-name-pattern "$test_name_pattern" $1
   fi
-  sleep 1
-  elapsed=$((elapsed + 1))
-done
+  teardown
+}
 
-NODE_ENV=test
-bun test
-
-# Stop and remove the Docker container after tests finish
-docker-compose -f ./docker-compose.yml down --volumes
+run $@
