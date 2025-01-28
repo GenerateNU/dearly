1424e47cd175841638cced29df37dc19acb73123
Josh Torre <torre.jo@northeastern.edu>
2025-01-27 19:53:16 -0500

    Initial ideas

---
+++ backend/src/services/notificationsService.ts
@@ -0,0 +1,40 @@
+import { createClient } from '@supabase/supabase-js'
+
+/**
+ * POST: a new post is made in the group
+ * LIKE: someone like your post
+ * COMMENT: someone comments on your post
+ */
+
+interface INotificationService {
+
+}
+
+
+class NotificationImpl{
+    constructor() {
+
+    }
+
+    subscribe() {
+        const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+        // Create a function to handle inserts
+        const handleInserts = (payload) => {
+        console.log('Change received!', payload)
+        }
+
+        // Listen to inserts
+        supabase
+        .channel('todos')
+        .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'todos' }, handleInserts)
+        .subscribe()
+
+    }
+}
+
+
+
+
+
+
