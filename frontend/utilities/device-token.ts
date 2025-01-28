7e8a416050a405554877665748c1999b02f5a323
mai <thisismainguyen@gmail.com>
2025-01-27 21:46:08 -0500

    refactor getExpoDeviceToken

--- frontend/utilities/device-token.ts
+++ frontend/utilities/device-token.ts
@@ -12,34 +12,41 @@ export const getExpoDeviceToken = async (): Promise<string | null> => {
   }
 
   try {
-    // setup notification channel for Android
-    if (Platform.OS === "android") {
-      await Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      });
-    }
+    await setUpAndroidNotificationChannel();
+    await requestNotificationPermission();
 
-    // request permissions if not granted
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      if (status !== "granted") {
-        console.error("Push notification permission denied.");
-        return null;
-      }
-    }
-
-    // get the Expo push token
     const projectId =
       Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+
     const { data: expoPushToken } = await Notifications.getExpoPushTokenAsync({ projectId });
 
     return expoPushToken;
   } catch (error) {
     console.error("Error getting Expo push token:", error);
     return null;
+  }
+};
+
+const setUpAndroidNotificationChannel = async () => {
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+};
+
+const requestNotificationPermission = async () => {
+  // request permissions if not granted
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Push notification permission denied.");
+      return null;
+    }
   }
 };
