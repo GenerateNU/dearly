61a22c9bd3495c69a5179fed9f4cd4f32d3df6fb
mai <thisismainguyen@gmail.com>
2025-02-01 13:46:22 -0500

    refactor routes

--- backend/src/routes/init.ts
+++ backend/src/routes/init.ts
@@ -6,11 +6,8 @@ import { getOpenAPISpecification } from "../utilities/docs";
 import { HEALTHCHECK } from "../types/api/routes/healthcheck";
 import { groupRoutes } from "../entities/groups/route";
 import { postRoutes } from "../entities/posts/route";
-import { memberRoutes } from "../entities/members/route";
-import { likeRoutes } from "../entities/likes/route";
 import { IS3Operations } from "../services/s3Service";
 import { MediaServiceImpl } from "../entities/media/service";
-import { mediaRoutes } from "../entities/media/route";
 import { commentsRoutes } from "../entities/comments/route";
 
 export const setUpRoutes = (
@@ -48,9 +45,6 @@ const apiRoutes = (db: PostgresJsDatabase, s3Service: IS3Operations): Hono => {
   api.route("/users", userRoutes(db, mediaService));
   api.route("/groups", groupRoutes(db, mediaService));
   api.route("/", postRoutes(db, mediaService));
-  api.route("/posts/:id/likes", likeRoutes(db, mediaService));
-  api.route("/groups/:id/media", mediaRoutes(mediaService));
-  api.route("/groups/:id/members", memberRoutes(db, mediaService));
   api.route("/", commentsRoutes(db, mediaService));
 
   return api;
