a052ce5bee148f99b70d361ffac6b1caced17508
mai <thisismainguyen@gmail.com>
2025-02-04 22:42:27 -0500

    i think this works?

--- backend/src/tests/helpers/mock.ts
+++ backend/src/tests/helpers/mock.ts
@@ -1,8 +1,23 @@
 import { mock, jest } from "bun:test";
 import { MOCK_SIGNED_URL } from "./test-constants";
+import { Expo, ExpoPushMessage, ExpoPushTicket } from "expo-server-sdk";
 
 mock.module("@aws-sdk/s3-request-presigner", () => {
   return {
     getSignedUrl: jest.fn().mockResolvedValue(MOCK_SIGNED_URL),
+  };
+});
+
+mock.module("expo-server-sdk", () => {
+  return {
+    Expo: class {
+      static isExpoPushToken = Expo.isExpoPushToken;
+      getPushNotificationReceiptsAsync = Expo.prototype.getPushNotificationReceiptsAsync;
+      
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      sendPushNotificationsAsync(messages: ExpoPushMessage[]): Promise<ExpoPushTicket[]> {
+        return Promise.resolve([]);
+      }
+    }
   };
 });
