522c78ff5ffe486e00a96c54845f719d2fcbf6df
mai <thisismainguyen@gmail.com>
2025-02-13 12:13:46 -0500

    cleanup

--- frontend/app/(auth)/index.tsx
+++ frontend/app/(auth)/index.tsx
@@ -1,14 +1,11 @@
 import { useAuth } from "@/auth/provider";
 import Box from "@/design-system/base/box";
 import Button from "@/design-system/components/ui/button";
-import HomeMenu from "@/design-system/components/ui/home-menu";
 import { Mode } from "@/types/mode";
 import { router } from "expo-router";
-import { useState } from "react";
 
 const Index = () => {
   const { setMode, mode } = useAuth();
-  const [category, setCategory] = useState<string>("Home");
 
   const toggleTheme = () => {
     setMode(mode === Mode.ADVANCED ? Mode.BASIC : Mode.ADVANCED);
@@ -26,7 +23,6 @@ const Index = () => {
       <Button label="Toggle Mode" onPress={toggleTheme} />
       <Button label="Login" onPress={() => router.push("/(auth)/login")} />
       <Button label="Register" onPress={() => router.push("/(auth)/register")} />
-      <HomeMenu categories={["Home", "Calendar"]} selected={category} setSelected={setCategory} />
     </Box>
   );
 };
