9628cbd1903d1e7a00e22aa71779f2f1f947991b
Zainab Imadulla <113566112+ZainabImadulla@users.noreply.github.com>
2025-02-13 09:56:40 -0500

    💬 feat: comment endpoints (#125)

--- backend/src/entities/comments/service.ts
+++ backend/src/entities/comments/service.ts
@@ -1,3 +1,8 @@
+import {
+  Comment,
+  CommentPagination,
+  CreateCommentPayload,
+} from "../../types/api/internal/comments";
 import { IDPayload } from "../../types/id";
 import { handleServiceError } from "../../utilities/errors/service-error";
 import { MediaService } from "../media/service";
@@ -5,6 +10,9 @@ import { CommentTransaction } from "./transaction";
 
 export interface CommentService {
   toggleLikeComment(payload: IDPayload): Promise<boolean>;
+  createComment(payload: CreateCommentPayload): Promise<Comment>;
+  deleteComment(payload: IDPayload): Promise<void>;
+  getComments(payload: CommentPagination): Promise<Comment[]>;
 }
 
 export class CommentServiceImpl implements CommentService {
@@ -21,5 +29,36 @@ export class CommentServiceImpl implements CommentService {
       return await this.commentTransaction.toggleLikeComment(payload);
     };
     return await handleServiceError(toggleLikeCommentImpl)();
+  }
+
+  async createComment(payload: CreateCommentPayload): Promise<Comment> {
+    const createCommentImpl = async () => {
+      return await this.commentTransaction.createComment(payload);
+    };
+    return await handleServiceError(createCommentImpl)();
+  }
+
+  async getComments(payload: CommentPagination): Promise<Comment[]> {
+    const getCommentsImpl = async () => {
+      const comments = await this.commentTransaction.getComments(payload);
+      const commentsWithURL = await Promise.all(
+        comments.map(async (comment) => {
+          if (comment.voiceMemo) {
+            const voiceMemoURL = await this.mediaService.getSignedUrl(comment.voiceMemo);
+            comment.voiceMemo = voiceMemoURL;
+          }
+          return comment;
+        }),
+      );
+      return commentsWithURL;
+    };
+    return await handleServiceError(getCommentsImpl)();
+  }
+
+  async deleteComment(payload: IDPayload): Promise<void> {
+    const deleteCommentsImpl = async () => {
+      return await this.commentTransaction.deleteComment(payload);
+    };
+    return await handleServiceError(deleteCommentsImpl)();
   }
 }
