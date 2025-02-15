affa4939d9365b8b68a2c900ba4b388d2ca03fe8
Mai Nguyen <123816878+in-mai-space@users.noreply.github.com>
2025-02-14 19:45:22 -0500

    frontend deployment + some frontend components (#130)
    Co-authored-by: Stone Liu <liu.sto@northeastern.edu>

---
+++ frontend/design-system/base/animated-box.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps, ReactNode } from "react";
+import Box from "./box";
+import { ViewProps } from "react-native";
+import Animated from "react-native-reanimated";
+import {
+  boxRestyleFunctions,
+  createRestyleComponent,
+  RestyleFunctionContainer,
+} from "@shopify/restyle";
+import { Theme } from "./theme";
+
+type BoxProps = ComponentProps<typeof Box>;
+type AnimatedViewProps = Partial<ViewProps> & {
+  children?: ReactNode;
+};
+type Props = BoxProps & AnimatedViewProps;
+
+export const AnimatedBox = createRestyleComponent<BoxProps & Omit<Props, keyof BoxProps>, Theme>(
+  boxRestyleFunctions as RestyleFunctionContainer<BoxProps, Theme>[],
+  Animated.View,
+);
