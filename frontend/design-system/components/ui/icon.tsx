51d7c5670312193f03cd65be267306aefd218993
mai <thisismainguyen@gmail.com>
2025-02-19 11:57:03 -0500

    add navbar

--- frontend/design-system/components/ui/icon.tsx
+++ frontend/design-system/components/ui/icon.tsx
@@ -4,34 +4,42 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import React from "react";
 import { Text } from "@/design-system/base/text";
 import { ResponsiveValue } from "@shopify/restyle";
+import { ColorName, ColorPalette } from "@/design-system/base/config/color";
 
 interface IconProps {
   name: MaterialIcon;
-  color: string;
+  color: ColorName;
   size: number;
   labelPosition?: "left" | "right" | "bottom" | "top";
   label?: string;
+  navbar?: boolean;
 }
 
-export const Icon: React.FC<IconProps> = ({ name, color, size, labelPosition, label }) => {
+export const Icon: React.FC<IconProps> = ({ name, color, size, labelPosition, label, navbar }) => {
   const positionStyles: Record<
     string,
     ResponsiveValue<"row" | "row-reverse" | "column" | "column-reverse", undefined>
   > = {
     left: "row",
     right: "row-reverse",
-    top: "column",
-    bottom: "column-reverse",
+    top: "column-reverse",
+    bottom: "column",
   };
 
   return (
     <Box
+      width="100%"
       justifyContent="center"
       alignItems="center"
+      gap="xs"
       flexDirection={positionStyles[labelPosition || "right"]}
     >
-      <MaterialCommunityIcons name={name} color={color} size={size} />
-      {label && <Text>{label}</Text>}
+      <MaterialCommunityIcons name={name} color={ColorPalette[color]} size={size} />
+      {label && (
+        <Text color={color} variant={navbar ? "navbar" : "button"}>
+          {label}
+        </Text>
+      )}
     </Box>
   );
 };
