366dfe064de943b769f7e259e1a56deae3321f60
Josh Torre <torre.jo@northeastern.edu>
2025-03-04 17:37:55 -0500

    Shows posted images

--- frontend/design-system/components/posts/masonry.tsx
+++ frontend/design-system/components/posts/masonry.tsx
@@ -2,27 +2,34 @@ import React from "react";
 import { MasonryFlashList } from "@shopify/flash-list";
 import { Box } from "@/design-system/base/box";
 import { Photo } from "./photo";
+import { BaseButton } from "@/design-system/base/button";
+import { Post } from "@/types/post";
 
 interface MasonryFeedProps {
-  data: string[];
+  posts: Post[];
 }
 
-export const MasonryList: React.FC<MasonryFeedProps> = ({ data }) => {
+export const MasonryList: React.FC<MasonryFeedProps> = ({ posts }) => {
   return (
     <Box flex={1} width="100%">
       <MasonryFlashList
-        data={data}
+        data={posts}
         numColumns={2}
         renderItem={({ item, index }) => (
-          <Box
-            width="100%"
-            height="100%"
-            padding="s"
-            paddingRight={index % 2 === 0 ? "s" : "none"}
-            paddingLeft={index % 2 !== 0 ? "s" : "none"}
+          <BaseButton
+            variant="text"
+            onPress={() => console.log("Sending you to the post", item.id)}
           >
-            <Photo image={item} />
-          </Box>
+            <Box
+              width="100%"
+              height="100%"
+              padding="s"
+              paddingRight={index % 2 === 0 ? "s" : "none"}
+              paddingLeft={index % 2 !== 0 ? "s" : "none"}
+            >
+              <Photo image={item.media![0]?.url!} />
+            </Box>
+          </BaseButton>
         )}
         estimatedItemSize={200}
       />
