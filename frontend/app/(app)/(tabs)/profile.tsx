7e5bdb5ef0e544915d9e26a82699652f56ace2f3
Josh Torre <torre.jo@northeastern.edu>
2025-03-04 00:32:22 -0500

    Starts of the profile page

--- frontend/app/(app)/(tabs)/profile.tsx
+++ frontend/app/(app)/(tabs)/profile.tsx
@@ -1,12 +1,82 @@
 import { Box } from "@/design-system/base/box";
 import { Text } from "@/design-system/base/text";
+import { useUserStore } from "@/auth/store";
+import { BaseButton } from "@/design-system/base/button";
+import { Icon } from "@/design-system/components/ui/icon";
+import { getUser } from "@/api/user";
+import { useQuery } from "@tanstack/react-query";
+import { Profile } from "@/design-system/components/ui/profile";
 
-const Profile = () => {
+const ProfilePage = () => {
   return (
-    <Box gap="xl" alignItems="center" justifyContent="center" backgroundColor="pearl" flex={1}>
-      <Text>Profile</Text>
+    <Box
+      gap="xl"
+      alignItems="center"
+      padding="l"
+      justifyContent="center"
+      backgroundColor="pearl"
+      flex={1}
+    >
+      <InfoBar />
+      <UserInfo />
     </Box>
   );
 };
 
-export default Profile;
+const InfoBar = () => {
+  const username = "";
+  const { group } = useUserStore();
+  const groupName = group ? group.name : "Unknown";
+  const onPressGroupName = () => {
+    console.log("shitter");
+  };
+
+  return (
+    <Box flexDirection="row" width={"100%"} justifyContent="space-between">
+      <BaseButton variant="text" onPress={onPressGroupName}>
+        <Box flexDirection="row" alignItems="center">
+          <Text variant="bodyLarge">{groupName}</Text>
+          <Icon name="arrow-down-drop-circle-outline" />
+        </Box>
+      </BaseButton>
+
+      <BaseButton
+        variant="text"
+        onPress={() => {
+          console.log("settings");
+        }}
+      >
+        <Box flex={1} flexDirection="row" alignItems="center" justifyContent="flex-end">
+          <Text fontSize={14}>settings</Text>
+          <Icon name="cog-outline" />
+        </Box>
+      </BaseButton>
+    </Box>
+  );
+};
+
+const UserInfo = () => {
+  const { userId } = useUserStore();
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["api", "v1", "users", userId],
+    queryFn: () => getUser(userId!),
+  });
+
+  return !isPending && !isError && data ? (
+    <Box width="100%" maxWidth="100%">
+      <Profile
+        username={data!.username}
+        profilePhoto={data!.profilePhoto ? data!.profilePhoto : ""}
+        bio={data!.bio ? data!.bio : ""}
+        birthday={data!.birthday ? data!.birthday : ""}
+        name={data!.name}
+      />
+    </Box>
+  ) : (
+    <Box>
+      <Text>Loading...</Text>
+    </Box>
+  );
+};
+export default ProfilePage;
