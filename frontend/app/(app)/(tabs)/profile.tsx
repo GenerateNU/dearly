d9c52662fdfbaf644d5ff1e539ee5ae7270c8c4f
Stone Liu <liu.sto@northeastern.edu>
2025-03-11 19:57:21 -0400

    Add button for deeplinking

--- frontend/app/(app)/(tabs)/profile.tsx
+++ frontend/app/(app)/(tabs)/profile.tsx
@@ -1,6 +1,7 @@
 import { useUserStore } from "@/auth/store";
 import { Box } from "@/design-system/base/box";
 import { TextButton } from "@/design-system/components/shared/buttons/text-button";
+import { useInvitations } from "@/hooks/api/invite";
 
 const Profile = () => {
   const { logout } = useUserStore();
@@ -8,6 +9,7 @@ const Profile = () => {
   return (
     <Box gap="xl" alignItems="center" justifyContent="center" backgroundColor="pearl" flex={1}>
       <TextButton variant="text" label="Logout" onPress={logout} />
+      <TextButton variant="text" label="Send Message" onPress={useInvitations} />
     </Box>
   );
 };
