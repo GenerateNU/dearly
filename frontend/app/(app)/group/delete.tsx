192132ccf8467b1ae78a952f5405a8093cbe5b7b
meganlai27 <lai.me@northeastern.edu>
2025-03-19 20:43:10 -0400

    Added deleteGroup

--- frontend/app/(app)/group/delete.tsx
+++ frontend/app/(app)/group/delete.tsx
@@ -1,20 +1,25 @@
 import { useUserStore } from "@/auth/store";
 import { SafeAreaView } from "react-native";
 import SimplePage from "@/design-system/components/shared/simple-page";
+import { deleteGroup } from "@/api/group";
+import { useDeleteGroup } from "@/hooks/api/group";
 
 const DeleteGroup = () => {
   const { group } = useUserStore();
-
+  
+  const { mutate: deleteGroup, isPending, error, isError} = useDeleteGroup(group?.id as string);
+  
   if (!group) return; // should never happen
+  
 
   return (
     <SafeAreaView className="flex-1 mt-[15%]">
       <SimplePage
         title="Delete Group?"
-        isLoading={false}
-        isError={false}
-        error={new Error()}
-        onPress={() => null}
+        isLoading={isPending}
+        isError={isError}
+        error={error}
+        onPress={() => deleteGroup}
         buttonLabel="Delete Group"
         description="Deleting a group cannot be undone. All photos uploaded in this group will deleted."
       />
