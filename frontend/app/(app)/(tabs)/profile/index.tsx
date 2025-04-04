3e05471aa893c6d1b749b62ce6d6615b010e1976
mai <thisismainguyen@gmail.com>
2025-04-03 21:42:49 -0400

    fix error for no group profile

--- frontend/app/(app)/(tabs)/profile/index.tsx
+++ frontend/app/(app)/(tabs)/profile/index.tsx
@@ -1,8 +1,18 @@
 import { useUserStore } from "@/auth/store";
+import { Box } from "@/design-system/base/box";
+import { EmptyHomePage } from "@/design-system/components/home/empty";
 import User from "@/design-system/components/profiles/user";
 
 const ProfilePage = () => {
-  const { userId } = useUserStore();
+  const { userId, group } = useUserStore();
+
+  if (!group) {
+    return (
+      <Box width="100%" flex={1} padding="m">
+        <EmptyHomePage />
+      </Box>
+    );
+  }
 
   return <User id={userId!} />;
 };
